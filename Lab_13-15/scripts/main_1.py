82949173e4fb2447afca0d5012c82c514e9f0256
Szymon-Budziak <szymonbudziak@gmail.com>
2024-06-09 22:44:30 +0200

    chore: scipts update

--- Lab_13-15/scripts/main_1.py
+++ Lab_13-15/scripts/main_1.py
@@ -2,8 +2,10 @@ import folium
 import geojson
 from shapely.wkt import loads
 
+# Initialize an empty folium map
 m = folium.Map()
 
+# Define the SQL query to find the longest river and counties that intersect with it
 query = """
 WITH LongestRiver AS (
 SELECT r.geom
@@ -17,13 +19,21 @@ WHERE SDO_ANYINTERACT (c.geom, lr.geom) =
 'TRUE'
 """
 
-r = loads(cursor.execute(query).fetchall())
+# Execute the SQL query and fetch all results
+# This returns the geometries of counties that intersect with the longest river in WKT format
+results = cursor.execute(query).fetchall()
+
+# Define the style for the GeoJSON features
 style = {"fillColor": "blue", "color": "red"}
 
 l = []
-for row in r:
+for row in results:
+    # Create a GeoJSON feature with the geometry and empty properties and add to list
     g = geojson.Feature(geometry=row[0], properties={})
     l.append(g)
 
+# Create a GeoJSON FeatureCollection from the list of features
 feature_collection = geojson.FeatureCollection(l)
+
+# Add the GeoJSON data to the folium map with the specified style
 folium.GeoJson(feature_collection, style_function=lambda x: style).add_to(m)
